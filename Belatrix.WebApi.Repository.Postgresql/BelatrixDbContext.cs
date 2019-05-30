f2f9a0778a15b86a3c2a9c3e546b6906b157dff9
jordinola <jordinola@JORDINOLA>
2019-05-30 09:13:50 -0500

    Estructura de proyecto

---
+++ Belatrix.WebApi.Repository.Postgresql/BelatrixDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Belatrix.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.WebApi.Repository.Postgresql
+{
+    public class BelatrixDbContext : DbContext
+    {
+        public BelatrixDbContext(DbContextOptions<BelatrixDbContext> opt)
+            :base(opt)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Belatrix.WebApi.Repository.Postgresql/Configurations/CustomerConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.WebApi.Repository.Postgresql.Configurations
+{
+    class CustomerConfig
+    {
+    }
+}
