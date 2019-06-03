203d0567b08ccf8231f66d66dfc4eeeba6dd5ea8
Miguel Alonso Salas Diaz <miguelsd_180594@outlook.com>
2019-06-03 17:55:45 -0500

    Generic repository.

---
+++ Belatrix.WebApi.Repository.PostgresSQL/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Belatrix.WebApi.Repository.PostgresSQL
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private BelatrixDbContext _context;
+        public GenericRepository(BelatrixDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Create(T entity)
+        {
+            _context.Add(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Delete(T entity)
+        {
+            _context.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<IEnumerable<T>> Read()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<bool> Update(T entity)
+        {
+            _context.Update(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
