fff531786b6869d4063fa09ba14e3d4ca7e08a97
Miguel Alonso Salas Diaz <miguelsd_180594@outlook.com>
2019-06-04 09:44:13 -0500

    controller partial

---
+++ Belatrix.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Belatrix.WebApi.Models;
+using Belatrix.WebApi.Repository;
+using Belatrix.WebApi.Repository.PostgresSQL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Belatrix.WebApi.Controllers
+{
+    public class CustomerController : ControllerBase
+    {
+        private readonly GenericRepository<Customer> _repository; 
+        public CustomerController(GenericRepository<Customer> repository)
+        {
+            _repository = repository;
+        }
+    }
+}
